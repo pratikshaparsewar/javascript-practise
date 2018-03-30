eeb358228885742a808340d5c9db773fd1891442
pratikshaparsewar <parsewarpratikshap@gmail.com>
2018-03-30 20:37:08 +0530

    xhr method done

--- app.js
+++ app.js
@@ -1,77 +1,51 @@
+document.getElementById('button').addEventListener('click', loadData);
 
-// Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
+function loadData() {
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
 
-// load event listensers
-loadEventListeners()
+  // OPEN
+  xhr.open('GET', 'data.txt', true);
 
-function loadEventListeners() {
-    form.addEventListener('submit', addTask);
+  // console.log('READYSTATE', xhr.readyState);
 
-    taskList.addEventListener('click' , removeTask);
-    clearBtn.addEventListener('click', clearTasks);
-    filter.addEventListener('keyup',filterTasks);
-}
-
-// Add Task
-function addTask(e) {
-    if(taskInput.value === '') {
-      alert('Add a task');
-    }
-  
-    // Create li element
-    const li = document.createElement('li');
-    // Add class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create new link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append the link to li
-    li.appendChild(link);
-  
-    // Append li to ul
-    taskList.appendChild(li);
-  
-    // Clear input
-    taskInput.value = '';
-  
-    e.preventDefault();
+  // Optional - Used for spinners/loaders
+  xhr.onprogress = function(){
+    console.log('READYSTATE', xhr.readyState);
   }
 
- function removeTask(e) {
-   if(e.target.parentElement.classList.contains('delete-item'))
-   {
-      if(confirm("are you sure?")) {
-    e.target.parentElement.parentElement.remove();
-}
-   }
-  
- }
-
-function clearTasks() {
-    taskList.innerHTML = '';
-}
- 
-function filterTasks(e) {
-const text = e.target.value.toLowercase();
-document.querySelectorAll('collection-item').forEach(
-    function(task) {
-        const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) != -1) {
-            task.style.display = 'blcok';
-        }
-        else {
-            task.style.display = 'none';
-            }
+  xhr.onload = function(){
+    console.log('READYSTATE', xhr.readyState);
+    if(this.status === 200) {
+      // console.log(this.responseText);
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
     }
-);
+  }
+
+  // xhr.onreadystatechange = function() {
+  //   console.log('READYSTATE', xhr.readyState);
+  //   if(this.status === 200 && this.readyState === 4){
+  //     console.log(this.responseText);
+  //   }
+  // }
+
+  xhr.onerror = function() {
+    console.log('Request error...');
+  }
+
+  xhr.send();
+
+
+    // readyState Values
+    // 0: request not initialized 
+    // 1: server connection established
+    // 2: request received 
+    // 3: processing request 
+    // 4: request finished and response is ready
+
+
+  // HTTP Statuses
+  // 200: "OK"
+  // 403: "Forbidden"
+  // 404: "Not Found"
 }
